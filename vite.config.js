8685aa4373ad7d8a4ff1b3da33a11d03e19d690b
shankaresh <89827937+shankareshBB@users.noreply.github.com>
2024-02-13 20:55:59 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 5175
+  },
+  build: {
+    outDir: "dist",
+    chunkSizeWarningLimit: 10000 // Increase the limit to 10000 kB (10 MB)
+  },
 })
